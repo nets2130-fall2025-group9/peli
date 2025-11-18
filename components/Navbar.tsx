381c8435098a31f23c503cfefa0e9625a7cd3d77
James Doh <dohj0109@seas.upenn.edu>
2025-11-18 15:21:48 -0500

    fix redirect url for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { UserButton } from "@clerk/nextjs";
 export const Navbar = () => {
   return (
     <div className="fixed top-0 z-50 w-full bg-background flex justify-between items-center gap-4 h-16 px-4 border-b">
-      <Link href="/dashboard" className="flex items-center gap-2">
+      <Link href="/" className="flex items-center gap-2">
         <Image src="/logo.png" alt="logo" width={32} height={32} />
         <h2 className="text-2xl font-bold">Peli</h2>
       </Link>
